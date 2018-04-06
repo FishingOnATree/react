2a021aee436668be217c15cb7cf9544fe018ff32
Raymond Sung <raysung@nopsec.com>
2018-04-06 00:05:07 -0400

    lesson 3-5 done.

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,42 +1,45 @@
 import React, { Component } from 'react';
-
-const contacts = [
- {
-   "id": "karen",
-   "name": "Karen Isgrigg",
-   "handle": "karen_isgrigg",
-   "avatarURL": "http://localhost:5001/karen.jpg"
- },
- {
-   "id": "richard",
-   "name": "Richard Kalehoff",
-   "handle": "richardkalehoff",
-   "avatarURL": "http://localhost:5001/richard.jpg"
- },
- {
-   "id": "tyler",
-   "name": "Tyler McGinnis",
-   "handle": "tylermcginnis",
-   "avatarURL": "http://localhost:5001/tyler.jpg"
- }
-];
-
-class User extends React.Component{
-  render() {
-    return (
-      <div>
-        <p>Name: {this.props.username}</p>
-        <p>A friend? {this.props.friend} </p>
-      </div>
-    );
-  }
-}
+import ListContacts from './ListContacts'
 
 class App extends Component {
+  state = {
+    contacts: [
+       {
+         "id": "karen",
+         "name": "Karen Isgrigg",
+         "handle": "karen_isgrigg",
+         "avatarURL": "http://localhost:5001/karen.jpg"
+       },
+       {
+         "id": "richard",
+         "name": "Richard Kalehoff",
+         "handle": "richardkalehoff",
+         "avatarURL": "http://localhost:5001/richard.jpg"
+       },
+       {
+         "id": "tyler",
+         "name": "Tyler McGinnis",
+         "handle": "tylermcginnis",
+         "avatarURL": "http://localhost:5001/tyler.jpg"
+       }
+    ],
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+  }
+
   render() {
     return (
       <div>
-        <User username="Abc" friend={true} ></User>
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
       </div>
     );
   }
