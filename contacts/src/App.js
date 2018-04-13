f5412041c6b48282de9f8c1752ba8657c6c2e5a0
Raymond Sung <raysung@nopsec.com>
2018-04-13 00:11:12 -0400

    add create form

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContacts from './CreateContacts'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
-    contacts: [],
-    screen: 'list'
+    contacts: []
   }
 
   componentDidMount() {
@@ -24,23 +24,34 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then((contact) => {
+      this.setState((currentState) => ({
+        contacts: currentState.contacts.concat([contact])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
-        {this.state.screen === 'list' && (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-            onNavigate={() => {
-              this.setState(() => ({screen: 'create'}))
+        <Route exact path='/' render={() => (
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+            />
+          )}
+        />
+        <Route path='/create' render = {({ history} ) => (
+          <CreateContacts
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
             }}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContacts />
-        )}
+        )} />
       </div>
-    );
+    )
   }
 }
 
--- contacts/src/CreateContacts.js
+++ contacts/src/CreateContacts.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContacts extends Component {
+  handleSummit = (e) => {
+    e.preventDefault()
+    const values = serializeForm(e.target, {hash: true})
+    console.log('values', values)
+    if (this.props.onCreateContact) {
+       this.props.onCreateContact(values)
+    }
+  }
   render() {
     return (
-      <div>Create Contact</div>
+      <div>
+        <Link
+          className='close-create-contact'
+          to='/'>
+            Close
+        </Link>
+        <form onSubmit={this.handleSummit} className='create-contact-form'>
+          <ImageInput
+            className='creagte-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}
+          />
+          <div className='create-contact-details'>
+            <input type='text' name='name' placeholder='Name' />
+            <input type='text' name='handle' placeholder='Handle' />
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
