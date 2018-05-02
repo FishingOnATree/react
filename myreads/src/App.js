b5634ca081d72c127765607c66153e5be5518d02
Raymond Sung <raysung@nopsec.com>
2018-05-02 09:08:32 +0800

    Moved all books into a proper structure

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -27,7 +27,7 @@ class Bookshelf {
     var index = -1;
     for (var i=0; i<this.books.length; i++) {
       var book = this.books[i];
-      if (target.title == book.title && target.author  == book.author) {
+      if (target.title === book.title && target.author === book.author) {
         index = i;
       }
     }
@@ -37,35 +37,53 @@ class Bookshelf {
   }
 }
 
+const bookshelfList = ['reading', 'want', 'read', 'none'];
+const bookshelfTitleList = ['Currently Reading', 'Want to Read', 'Read', 'Not categorized'];
 class Library {
-  constructor() {
-    this.bookshelves = []
-    this.bookshelves.push(new Bookshelf('reading', 'Currently Reading'))
-    this.bookshelves.push(new Bookshelf('want', 'Want to Read'))
-    this.bookshelves.push(new Bookshelf('read', 'Read'))
-    this.bookshelves.push(new Bookshelf('none', 'Not categorized'))
+  constructor(bookshelfList, bookshelfTitleList) {
+    this.bookshelves = [bookshelfList.length];
+    for (var i=0; i<bookshelfList.length; i++) {
+      this.bookshelves[i] = new Bookshelf(bookshelfList[i], bookshelfTitleList[i]);
+    }
+  }
+
+  addBook(book, targetShelf) {
+    this.bookshelves.forEach((shelf) => {
+      if (shelf.key === targetShelf) {
+        shelf.addBook(book)
+      }
+    });
+  }
+
+  moveBook(book, fromShelf, targetShelf) {
+    this.bookshelves.forEach((shelf) => {
+      if (shelf.key === fromShelf) {
+        shelf.removeBook(book);
+      } else if (shelf.key === targetShelf) {
+        shelf.addBook(book);
+      }
+    });
   }
 }
-const book1 = new Book('To Kill a Mockingbird', 'Harper Lee', 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api');
-const book2 = new Book("Ender's Game", 'Orson Scott Card', 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api');
-const book3 = new Book("1776", 'David', 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api');
-const book4 = new Book("the Hobbit", 'Tolkien', 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api');
-const book5 = new Book("Harry Potter and the Sorcerer's Stone1", 'J.K. Rolling', 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api');
-const book6 = new Book("Harry Potter and the Sorcerer's Stone2", 'J.K. Rolling', 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api');
-//const bookshelf1 = [book1, book2];
-//const bookshelf2 = [book3, book4];
-//const bookshelf3 = [book5, book6];
-const bookshelf1 = new Bookshelf('reading', 'Currently Reading');
-bookshelf1.addBook(book1);
-bookshelf1.addBook(book2);
-const bookshelf2 = new Bookshelf('want', 'Want to Read');
-bookshelf2.addBook(book3);
-bookshelf2.addBook(book4);
-const bookshelf3 = new Bookshelf('read', 'Read');
-bookshelf3.addBook(book5);
-bookshelf3.addBook(book6);
 
-console.log(book1.authors);
+var myLib = new Library(bookshelfList, bookshelfTitleList);
+
+function populateExamples() {
+  var book1 = new Book('To Kill a Mockingbird', 'Harper Lee', 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api');
+  var book2 = new Book("Ender's Game", 'Orson Scott Card', 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api');
+  var book3 = new Book("1776", 'David', 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api');
+  var book4 = new Book("the Hobbit", 'Tolkien', 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api');
+  var book5 = new Book("Harry Potter and the Sorcerer's Stone1", 'J.K. Rolling', 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api');
+  var book6 = new Book("Harry Potter and the Sorcerer's Stone2", 'J.K. Rolling', 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api');
+  myLib.addBook(book1, 'reading');
+  myLib.addBook(book2, 'reading');
+  myLib.addBook(book3, 'want');
+  myLib.addBook(book4, 'want');
+  myLib.addBook(book5, 'read');
+  myLib.addBook(book6, 'read');
+}
+populateExamples();
+console.log(myLib);
 
 class BooksApp extends React.Component {
   state = {
@@ -109,9 +127,11 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <BookshelfDisplay bookshelf={bookshelf1} />
-                <BookshelfDisplay bookshelf={bookshelf2} />
-                <BookshelfDisplay bookshelf={bookshelf3} />
+                {myLib.bookshelves
+                  .map((bookshelf) => (
+                    <BookshelfDisplay bookshelf={bookshelf} />
+                  ))
+                }
               </div>
             </div>
             <div className="open-search">
