503ac2f8987149ceb27ed70b9e4d1dbb7456fd30
Raymond Sung <raysung@nopsec.com>
2018-05-07 06:55:10 +0800

    Updated search panel to use React Route

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './App.css'
 import BookshelfDisplay from './BookshelfDisplay'
 import {Book, Library} from './objects.js'
+import { Link, Route } from 'react-router-dom'
 
 const bookshelfList = [['reading', 'Currently Reading'],
                        ['want', 'Want to Read'],
@@ -27,13 +28,6 @@ function populateExamples() {
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     library: myLib
   }
 
@@ -50,10 +44,10 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route path='/add' render = {({ history} ) => (
           <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <a className="close-search" onClick={() => history.push('/')}>Close</a>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -71,7 +65,8 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        )} />
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -91,10 +86,10 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to='/add'>Add a book</Link>
             </div>
           </div>
-        )}
+        )} />
       </div>
     )
   }
