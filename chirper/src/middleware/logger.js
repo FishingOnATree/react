98d26ffb670f3a32993c334d9ce7975d7c8e851a
FishingOnATree <raysung78@gmail.com>
2018-05-27 20:32:09 -0400

    handle load init data

--- chirper/src/middleware/logger.js
+++ chirper/src/middleware/logger.js
@@ -1,9 +1,10 @@
-const logger = (sotre) => (next) => (action) => {
+const logger = (store) => (next) => (action) => {
   console.group(action.type)
     console.log(action)
     const returnValue = next(action)
     console.log(store.getState())
   console.groupEnd()
+  return returnValue
 }
 
 export default logger;
