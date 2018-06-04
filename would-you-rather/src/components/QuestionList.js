3deef2adb4371573f8a083a62c2bc9facd80569e
Raymond Sung <raysung@nopsec.com>
2018-06-03 22:40:48 -0400

    Make switch answered/unawsered link work

--- would-you-rather/src/components/QuestionList.js
+++ would-you-rather/src/components/QuestionList.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { formatDate } from '../utils/utils'
+import { updateViewAnswered } from '../actions/ViewAnswered'
 
 class QuestionList extends Component {
-  viewAnsweredPage = (viewAnswered) => {
-    // TODO: how to switch?
+  viewAnsweredPage = (newChoice) => {
+    console.log("new choice: " + newChoice)
+    this.setState({
+      viewAnswered: newChoice
+    })
   }
 
   render () {
     console.log(this.props);
-    const { user, questions, questionIds, viewAnswered } = this.props;
+    const { user, questions, questionIds, viewAnswered, dispatch } = this.props;
     return (
       <div className='contents'>
         <h3>QuestionList - {viewAnswered ? "Answered" : "Unanswered"}</h3>
         <div>
-          <a onClick={() => this.viewAnsweredPage(false)}>View Unanswered</a> |
-          <a onClick={() => this.viewAnsweredPage(true)}>View Answered</a>
+          <a onClick={() => dispatch(updateViewAnswered(false))}>View Unanswered</a> |
+          <a onClick={() => dispatch(updateViewAnswered(true))}>View Answered</a>
         </div>
         <ul>
           { questionIds.filter((id) => (id in user.answers === viewAnswered))
@@ -32,18 +36,15 @@ class QuestionList extends Component {
   }
 }
 
-function mapStateToProps({ authedUser, users, questions }, { viewAnswered }) {
+function mapStateToProps({ authedUser, users, questions, viewAnswered }) {
+  console.log("mapStateToProps")
   const user = users[authedUser];
-  console.log("viewAnswered init: " + viewAnswered);
-  if (viewAnswered === undefined) {
-    viewAnswered = false;
-  }
   return {
     user,
     questions,
     questionIds: Object.keys(questions)
       .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
-    viewAnswered: viewAnswered
+    viewAnswered
   }
 }
 
--- would-you-rather/src/reducers/index.js
+++ would-you-rather/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import authedUser from './AuthedUser'
 import questions from './Questions'
 import users from './Users'
+import viewAnswered from './ViewAnswered'
 import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
   authedUser,
   users,
   questions,
+  viewAnswered,
   loadingBar: loadingBarReducer
 })
