efd67e0d58c677b02a3d75e30570d2332c41f258
Raymond Sung <raysung@nopsec.com>
2018-06-03 22:45:26 -0400

    updated to remove unnecessary update

--- would-you-rather/src/components/QuestionList.js
+++ would-you-rather/src/components/QuestionList.js
@@ -4,22 +4,21 @@ import { formatDate } from '../utils/utils'
 import { updateViewAnswered } from '../actions/ViewAnswered'
 
 class QuestionList extends Component {
-  viewAnsweredPage = (newChoice) => {
-    console.log("new choice: " + newChoice)
-    this.setState({
-      viewAnswered: newChoice
-    })
+  updateViewAnsweredChoice = (choice) => {
+    if (this.props.viewAnswered !== choice) {
+      this.props.dispatch(updateViewAnswered(choice))
+    }
   }
 
   render () {
     console.log(this.props);
-    const { user, questions, questionIds, viewAnswered, dispatch } = this.props;
+    const { user, questions, questionIds, viewAnswered } = this.props;
     return (
       <div className='contents'>
         <h3>QuestionList - {viewAnswered ? "Answered" : "Unanswered"}</h3>
         <div>
-          <a onClick={() => dispatch(updateViewAnswered(false))}>View Unanswered</a> |
-          <a onClick={() => dispatch(updateViewAnswered(true))}>View Answered</a>
+          <a onClick={() => this.updateViewAnsweredChoice(false)}>View Unanswered</a> |
+          <a onClick={() => this.updateViewAnsweredChoice(true)}>View Answered</a>
         </div>
         <ul>
           { questionIds.filter((id) => (id in user.answers === viewAnswered))
