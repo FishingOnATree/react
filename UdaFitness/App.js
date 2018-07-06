22bd7bd2b77a33143d295900d461262e5cccedc9
FishingOnATree <raysung78@gmail.com>
2018-07-05 23:37:34 -0400

    Added history

--- UdaFitness/App.js
+++ UdaFitness/App.js
@@ -7,6 +7,7 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 import AddEntry from './components/AddEntry';
+import History from './components/History';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
@@ -17,7 +18,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     );
