b1f9c688f70733f31af3dc57f7fc230568ff4bf3
Raymond Sung <raysung@nopsec.com>
2018-07-21 09:16:17 -0400

    get basic navigation working

---
+++ mobile-flashcards/components/ShowDeck.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import TextButton from './TextButton'
+
+class ShowDeck extends Component {
+
+  render() {
+    const navigate = this.props.navigation.navigate
+    const { title } = this.props.navigation.state.params
+    const deck = this.props.decks[title]
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{deck.title}</Text>
+        <Text style={styles.cards}>{deck.questions.length} {deck.questions.length>1 ? 'quizzes':'quiz'}</Text>
+        <TextButton /*onPress={() => navigate('AddQuiz', { deck })}*/ >
+          Add Quiz
+        </TextButton>
+        <TextButton
+          /*onPress={() => navigate('QuizView', { deck })}*/ >
+          Start Quiz
+        </TextButton>
+      </View>
+    )
+  }
+}
+
+function mapStateToProps(deck) {
+  return {
+    decks
+  }
+}
+
+export default connect(mapStateToProps)(ShowDeck)
