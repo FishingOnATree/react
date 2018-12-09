013f1c08134255ae196c81e86dac568aafb55d8c
Raymond S <raysung78@gmail.com>
2018-12-09 10:29:47 -0500

    initial commit

---
+++ guess_number/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Guess Number</h1>
+          <div>control panel</div>
+          <div>display panel</div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
